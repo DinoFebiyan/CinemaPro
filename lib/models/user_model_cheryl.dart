e8c89af6098b9440137f52c82deabb2cb650a181
cherylaurellya <cherylaurellya577@gmail.com>
2025-11-30 06:13:09 +0700

    user_cheryl

--- lib/models/user_model_cheryl.dart
+++ lib/models/user_model_cheryl.dart
@@ -1,15 +1,31 @@
-// class UserModelCheryl {
-//   final String uid;
-//   final String email;
-//   final String username;
-//   final int balance;
+class UserModelCheryl {
+  final String uid;
+  final String email;
+  final String username;
+  final int balance;
 
-//   UserModelCheryl ({
-//     required this.uid,
-//     required this.email,
-//     required this.username,
-//     required this.balance
-//   });
+  UserModelCheryl ({
+    required this.uid,
+    required this.email,
+    required this.username,
+    required this.balance
+  });
 
-//   Map<String, dynamic>
-// }
+  Map<String, dynamic> toMapCheryl() {
+    return {
+      'uid' : uid,
+      'email' : email,
+      'username' : username,
+      'balance' : balance
+    };
+  }
+
+  factory UserModelCheryl.fromMapCheryl(Map<String, dynamic> map) {
+    return UserModelCheryl(
+      uid:map['uid'] ?? '', 
+      email: map['email'] ?? '', 
+      username: map['username'] ?? '', 
+      balance: (map['balance'] ?? 0).toInt(),
+      );
+  }
+}
