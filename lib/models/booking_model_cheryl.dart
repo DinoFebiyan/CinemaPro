078f346507bd49ab3073f1650995bdca6770efbf
cherylaurellya <cherylaurellya577@gmail.com>
2025-12-01 12:38:18 +0700

    perbaikan booking

--- lib/models/booking_model_cheryl.dart
+++ lib/models/booking_model_cheryl.dart
@@ -29,13 +29,20 @@ class BookingModelCheryl {
   }
 
   factory BookingModelCheryl.fromMap_Cheryl(Map<String, dynamic> map, String id) {
+    DateTime safeDate;
+    if (map['booking_date'] != null && map['booking_date'] is Timestamp) {
+      safeDate = (map['booking_date'] as Timestamp).toDate();
+    } else {
+      print("WARNING: Tanggal error/null untuk booking $id. Menggunakan waktu sekarang.");
+      safeDate = DateTime.now();
+    }
     return BookingModelCheryl(
       bookingId: id, 
-      userId: map ['userId'] ?? '', 
-      movieTitle: map ['movieTitle'] ?? '' , 
+      userId: map ['user_id'] ?? '', 
+      movieTitle: map ['movie_title'] ?? '' , 
       seats: List<String>.from(map['seats'] ?? []), 
-      totalPrice: map ['totalPrice']?.toInt() ?? 0, 
-      bookingDate: (map['bookingDate'] as Timestamp).toDate(),
+      totalPrice: map ['total_price']?.toInt() ?? 0, 
+      bookingDate:safeDate,
       );
   }
 }
